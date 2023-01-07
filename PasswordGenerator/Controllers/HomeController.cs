3545d7c3f9bacecf5b22e1d28e1ece23407b4ddf
Luiz Silva <luiz1082@hotmail.com>
2023-01-07 15:11:38 -0300

    Add project files.

---
+++ PasswordGenerator/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordGenerator.Models;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace PasswordGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Generate(PasswordGen password)
+        {
+
+            password.Size = password.Size != null ? password.Size < 7 ? 7 : password.Size : 7;
+            int Size = password.Size.Value;
+
+            Random random = new Random();
+            PasswordGen Pass = new PasswordGen();
+
+            string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ 0123456789!@#$%&*()Ç[](){}-+/,.~^º£¢Ç|\\";
+
+            string passwordInCaps = new string(Enumerable.Repeat(chars, Size).Select(s => s[random.Next(s.Length)]).ToArray()).ToLower();
+
+            var scrambleCaps = passwordInCaps.Select(x => random.Next() % 2 == 0 ? (char.IsUpper(x) ? x.ToString().ToLower().First() : x.ToString().ToUpper().First()) : x);
+            var resultScrambleCaps = new string(scrambleCaps.ToArray());
+
+            Pass.Password = !string.IsNullOrEmpty(resultScrambleCaps) ? resultScrambleCaps.ToString() : "";
+            Pass.Size = Size;
+
+            return View("Index", Pass);
+        }
+    }
+}
---
+++ PasswordGenerator/Models/PasswordGen.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PasswordGenerator.Models
+{
+    public class PasswordGen
+    {
+        [Display(Name ="Size")]
+        [Range(7, 25, ErrorMessage = "Can only be between 7 .. 25")]
+        public int? Size { get; set; }
+        public string? Password { get; set; }
+    }
+}
